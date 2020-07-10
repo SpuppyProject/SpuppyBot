5e999c59a2e878755e32d804627019ff013afd4f
bluepen <bluepenko@gmail.com>
2020-07-10 14:39:18 +0900

    4.0-beta1 릴리즈

--- src/main/kotlin/com/github/yeoj34760/spuppybot/commands/Info.kt
+++ src/main/kotlin/com/github/yeoj34760/spuppybot/commands/Info.kt
@@ -1,6 +1,7 @@
 package com.github.yeoj34760.spuppybot.commands
 
 import com.github.yeoj34760.spuppybot.VERSION
+import com.github.yeoj34760.spuppybot.other.DiscordColor
 import com.jagrosh.jdautilities.command.Command
 import com.jagrosh.jdautilities.command.CommandEvent
 import net.dv8tion.jda.api.EmbedBuilder
@@ -12,13 +13,14 @@ object Info : Command() {
     }
     override fun execute(event: CommandEvent) {
         val embed = EmbedBuilder()
-                .setAuthor("SpuppyBot", null, "")
+                .setAuthor("SpuppyBot")
                 .setDescription("스퍼피봇은 뮤직 기능을 제공하는 봇입니다.")
-                .addField("developer", "ImperskyPenguin#1090", true)
-                .addField("version", VERSION, true)
-                .addField("open source", "[GITHUB](https://github.com/yeoj34760/SpuppyBot)", true)
-                .addField("License", "[GPLv3](https://github.com/yeoj34760/SpuppyBot/blob/master/LICENSE)", true)
                 .addField("servers", event.jda.guilds.size.toString(), true)
+                .addField("License", "[GPLv3](https://github.com/yeoj34760/SpuppyBot/blob/master/LICENSE)", true)
+                .addField("open source", "[GITHUB](https://github.com/yeoj34760/SpuppyBot)", true)
+                .addField("version", VERSION, true)
+                .addField("developer", "ImperskyPenguin#1090", true)
+                .setColor(DiscordColor.BLUE)
                 .build()
 
         event.reply(embed)
