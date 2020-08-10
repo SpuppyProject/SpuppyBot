7a7191cd8ba739f1400e65499fb6063e8f43c47c
yeoj34760 <yeoj34760@github.com>
2020-08-11 06:33:59 +0900

    주석 추가

--- src/main/kotlin/com/github/yeoj34760/spuppybot/music/GuildManager.kt
+++ src/main/kotlin/com/github/yeoj34760/spuppybot/music/GuildManager.kt
@@ -6,6 +6,9 @@ import net.dv8tion.jda.api.managers.AudioManager
 object GuildManager {
     val tracks = HashMap<Long, PlayerControl>()
 
+    /**
+     * 해당 길드전용 플레이어 컨트롤을 반환합니다. 생성되어 있지 않을 경우 null로 반환합니다.
+     */
    operator fun get(id: Long): PlayerControl? = tracks[id]
 
     /**
@@ -15,13 +18,14 @@ object GuildManager {
         val playerControl: PlayerControl
         if (!tracks.containsKey(id)) {
             val player = playerManager.createPlayer()
+            //볼륨 값 70로 설정
             player.volume = 70
+            //오디오매니저에 플레이어 등록
             audioManager.sendingHandler = PlayerSendHandler(player)
+            //해당 길드전용 플레이어컨트롤 생성
             playerControl = PlayerControl(player, audioManager)
             player.addListener(playerControl)
             tracks[id] = playerControl
         }
     }
-
-    var connected = false
 }
