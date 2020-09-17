ba240fb5bc30cf5dcc952c6d7d53fcad73fc168d
bluepen <31067876+yeoj34760@users.noreply.github.com>
2020-09-18 00:06:22 +0900

    box 명령어 대응 코드추가

--- src/main/kotlin/com/github/yeoj34760/spuppybot/commands/box/Box.kt
+++ src/main/kotlin/com/github/yeoj34760/spuppybot/commands/box/Box.kt
@@ -7,6 +7,6 @@ import com.github.yeoj34760.spuppy.command.CommandSettings
 @CommandSettings(name = "box")
 object Box : Command() {
     override fun execute(event: CommandEvent) {
-        println(event.content)
+        event.channel.sendMessage("박스 명령어를 알고 싶다면 `http://spuppy.ml/spuppybot/help`에 확인해보세요!").queue()
     }
 }
