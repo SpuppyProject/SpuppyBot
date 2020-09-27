29054199d9b69ffa6bebd555d651bbcde072f929
bluepen <31067876+yeoj34760@users.noreply.github.com>
2020-09-27 16:53:35 +0900

    가입 명령어 수정

--- src/main/kotlin/com/github/yeoj34760/spuppybot/commands/other/Agree.kt
+++ src/main/kotlin/com/github/yeoj34760/spuppybot/commands/other/Agree.kt
@@ -4,41 +4,55 @@ import com.github.yeoj34760.spuppy.command.Command
 import com.github.yeoj34760.spuppy.command.CommandEvent
 import com.github.yeoj34760.spuppy.command.CommandSettings
 import com.github.yeoj34760.spuppybot.other.DiscordColor
-import com.github.yeoj34760.spuppybot.settings
 import com.github.yeoj34760.spuppybot.sql.spuppydb.UserDBController
 import com.github.yeoj34760.spuppybot.waiter
 import net.dv8tion.jda.api.EmbedBuilder
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent
 import java.util.concurrent.TimeUnit
 
 @CommandSettings(name = "agree", aliases = ["가입"])
 object Agree : Command() {
+    val text = """
+        가입하시기 전에 아래에 있는 
+        이용약관, 개인정보처리방침을 읽어주시고
+        동의하신다면 ⭕ 이모티콘을 눌러해주세요.
+    """.trimIndent()
+
     override fun execute(event: CommandEvent) {
         if (UserDBController.checkUser(event.author.idLong)) {
             event.channel.sendMessage("이미 가입되어 있네요!").queue()
             return
         }
 
-        val embed = EmbedBuilder().setAuthor("SpuppyBot").setDescription("가입하시기 전에 아래에 있는 \n이용약관, 개인정보처리방침을 읽어주시고\n동의하신다면 `${settings.prefix}동의` 입력해주세요.")
+        val embed = EmbedBuilder().setAuthor("SpuppyBot").setDescription(text)
                 .setColor(DiscordColor.YELLOW)
                 .addField("이용약관", "[확인](http://spuppy.ml/tos/)", true)
                 .addField("개인정보처리방침", "[확인](http://spuppy.ml/privacy_policy/)", true)
                 .build()
 
-        event.channel.sendMessage(embed).queue()
+        val message = event.channel.sendMessage(embed).complete()
+        message.addReaction("⭕").complete()
+        message.addReaction("❌").complete()
 
-        waiter.waitForEvent(MessageReceivedEvent::class.java, { e ->
-            e.author == event.author
-                    && e.channel == event.channel
-                    && e.message != event.message
-        }, { e ->
-            if (e.message.contentRaw == "${settings.prefix}동의") {
-                UserDBController.addUser(e.author.idLong)
-                event.channel.sendMessage("가입완료!").queue()
-            } else
-                return@waitForEvent
+
+        waiter.waitForEvent(MessageReactionAddEvent::class.java, {
+            it.messageId == message.id &&
+                    event.author.id == it.userId &&
+                    (it.reactionEmote.emoji == "⭕" ||
+                            it.reactionEmote.emoji == "❌")
+        }, {
+            message.delete().complete()
+
+            when (it.reactionEmote.emoji) {
+                "⭕" -> {
+                    UserDBController.addUser(event.author.idLong)
+                    event.channel.sendMessage("가입되었어요! 환영해요!").complete()
+                }
+                "❌" -> event.channel.sendMessage("취소되었어요, 만약 가입할 마음이 생긴다면 다시 불러와주세요!").complete()
+            }
+
         }, 1, TimeUnit.MINUTES) {
-            event.channel.sendMessage("시간 초과됨").queue()
+            event.channel.sendMessage("1분 지났어요. 만약 깜빡했다면 다시 불러와주세요").complete()
         }
     }
 }
--- src/main/kotlin/com/github/yeoj34760/spuppybot/commands/other/Cancel.kt
+++ src/main/kotlin/com/github/yeoj34760/spuppybot/commands/other/Cancel.kt
@@ -5,12 +5,18 @@ import com.github.yeoj34760.spuppy.command.CommandEvent
 import com.github.yeoj34760.spuppy.command.CommandSettings
 import com.github.yeoj34760.spuppybot.sql.spuppydb.UserBoxDBController
 import com.github.yeoj34760.spuppybot.sql.spuppydb.UserDBController
+import com.github.yeoj34760.spuppybot.sql.spuppydb.UserMoneyDBController
 
 @CommandSettings(name = "cancel", aliases = ["탈퇴"])
 object Cancel : Command() {
     override fun execute(event: CommandEvent) {
+        if (!UserDBController.checkUser(event.author.idLong)) {
+            event.channel.sendMessage("가입되어 있지 않습니다!").queue()
+            return
+        }
         UserBoxDBController.delAllUserBox(event.author.idLong)
+        UserMoneyDBController.delMoneyUser(event.author.idLong)
         UserDBController.delUser(event.author.idLong)
-        event.channel.sendMessage("탈퇴 됨").queue()
+        event.channel.sendMessage("탈퇴되었어요. 다시 돌아오길 바랄게요.").queue()
     }
 }
