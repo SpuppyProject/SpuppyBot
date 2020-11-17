337698a2af31b0f44c16b6b0788dcf1fe72fefb6
bluepen <31067876+yeoj34760@users.noreply.github.com>
2020-11-18 00:04:07 +0900

    missing file add

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "1.4.10"
+    kotlin("plugin.serialization") version "1.4.10"
+    application
+}
+
+repositories {
+    jcenter()
+    maven(url = "https://jitpack.io")
+}
+
+dependencies {
+    //The discord related libraries
+    implementation("net.dv8tion:JDA:4.2.0_217")
+    implementation("com.github.Devoxin:lavaplayer:1.3.61")
+
+    //other
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    // Define the main class for the application.
+    mainClassName = "com.github.yeoj34760.bot.BotAt"
+}
